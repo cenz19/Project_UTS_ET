b6ae2394ec298e884710d4b7287e519bd9d66094
cenz19 <vincenthadinata19@gmail.com>
2024-04-30 09:28:20 +0700

    login -2

--- memorimage_160421072_160421017/lib/main.dart
+++ memorimage_160421072_160421017/lib/main.dart
@@ -12,6 +12,7 @@ void main() {
 
   // runApp(const MyApp());
   WidgetsFlutterBinding.ensureInitialized();
+
   checkUser().then((String result) {
     if (result == '')
       runApp(LoginForm());
@@ -30,9 +31,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        // 'test': (context) => Test(),
+        'login': (context) => LoginForm(),
       },
-      title: 'Memorimage',
+      title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -49,7 +50,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -77,6 +78,9 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _currentIndex = 0;
+  // final List<Widget> _screens = [Home(), Search(), History()];
+  final List<String> _judul = ['Home', 'Search', 'History'];
 
   void _incrementCounter() {
     setState(() {
@@ -105,44 +109,51 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(_judul[_currentIndex]),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // body: _screens[_currentIndex],
       drawer: myDrawer(),
+      bottomNavigationBar: myBottomNavBar(),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  BottomNavigationBar myBottomNavBar() {
+    return BottomNavigationBar(
+        currentIndex: _currentIndex,
+        fixedColor: Colors.teal,
+        items: [
+          BottomNavigationBarItem(
+            label: "Home",
+            icon: Icon(
+              Icons.home,
+              size: 40,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "Search",
+            icon: Icon(
+              Icons.search,
+              size: 40,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "History",
+            icon: Icon(
+              Icons.history,
+              size: 40,
+            ),
+          ),
+          // BottomNavigationBarItem(
+          //   label: "Buy",
+          //   icon: Icon(Icons.shopping_cart),
+          // ),
+        ],
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        });
   }
 
   Drawer myDrawer() {
